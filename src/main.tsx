a1148a9f66d5376bee0177199739b39edc49f027
hiimsewek <102280@stud.uz.zgora.pl>
2024-01-01 11:27:49 +0100

    Set up redux store

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GlobalStyles, ThemeProvider } from "@mui/material";
 import { globalStyles, theme } from "@styles";
+import { Provider } from "react-redux";
+import { store } from "@store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles styles={globalStyles} />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
