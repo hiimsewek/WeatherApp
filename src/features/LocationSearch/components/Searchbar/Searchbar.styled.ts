ecb25c660d6d3f7bb75be65cafc623c4d240e549
hiimsewek <102280@stud.uz.zgora.pl>
2024-01-04 22:10:16 +0100

    Remove searchbar width animation

--- src/features/LocationSearch/components/Searchbar/Searchbar.styled.ts
+++ src/features/LocationSearch/components/Searchbar/Searchbar.styled.ts
@@ -3,10 +3,9 @@ import { Input, type InputProps } from "@components";
 
 export const StyledSearchbar = styled(Input)<InputProps & { expand: number }>(
   ({ expand }) => ({
-    width: expand ? "100%" : 0,
+    display: expand ? "flex" : "none",
     opacity: expand ? 1 : 0,
     overflow: "hidden",
-    transition: "width .2s linear",
     cursor: expand ? "auto" : "context-menu",
     animation: "none",
   })
